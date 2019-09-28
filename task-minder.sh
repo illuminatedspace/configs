3c346163d7ab3f8fa46e6ca13b3e06fa6477fbf3
Liz Phillips <lizkristinaphillips@gmail.com>
2019-09-28 10:41:17 -0400

    update task minder to pass all arguments

--- task-minder.sh
+++ task-minder.sh
@@ -4,9 +4,9 @@
 set -e
 
 # first argument passed to shell script from command line
-CMD=$1
+CMD="$@"
 
-echo "Running command $CMD"
-echo "Will say 'done' if completed, and 'error' if not completed"
+echo "I'll mind command $CMD so you don't have to!"
+echo "I'll say 'done' if completed, and 'error' if non-zero exit"
 
 ($CMD && say done) || say error
