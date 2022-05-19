946c4afcb47155bd3b6f273ae1af07aa9c16e03c
Liz P <lizkristinaphillips@gmail.com>
2022-05-19 14:37:16 -0400

    Update task-minder.sh

--- task-minder.sh
+++ task-minder.sh
@@ -1,14 +1,21 @@
-#!/bin/bash
+#!/bin/zsh
 
 # only works on MacOS
 
 # exit immediately if command exits with a non 0 status (error)
 set -e
 
-# first argument passed to shell script from command line
-CMD="$@"
+# source shell configs so aliases work
+# https://unix.stackexchange.com/questions/1496/why-doesnt-my-bash-script-recognize-aliases
+shopt -s expand_aliases
+source ~/Repositories/configs/aliases.sh
 
+# first argument passed to shell script from command line
+CMD="$*"
+
+say minding
 echo "I'll mind command $CMD so you don't have to!"
 echo "I'll say 'done' if completed, and 'error' if non-zero exit"
+echo $CMD
 
 ($CMD && say done) || say error
